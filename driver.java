1a291953d66d5b5e3489c033a4b6d6b0827fe84e
ElizaHlglh <ewu4@stuy.edu>
2019-02-01 09:43:56 -0500

    driver.java

--- driver.java
+++ driver.java
@@ -1,7 +1,9 @@
 public class driver{
   public static void main(String[] args){
     System.out.println("Testing sqrt():");
-    System.out.println();
     System.out.println("sqrt(2), Expecting: 1.4142.   Getting: " + recursion.sqrt(2,0.001));
+    System.out.println();
+    System.out.println("Testing sqrt():");
+    System.out.println("fib(5), Expecting: 8.   Getting: " + recursion.fib(5));
   }
 }
